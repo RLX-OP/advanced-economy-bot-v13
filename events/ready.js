a61c48017c8f1e93690768303425b5855fa329d1
Mathiscool <82745607+testbot-github@users.noreply.github.com>
2021-12-21 18:30:09 +0530

    Version Logging

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,9 @@
+const Discord = require("discord.js");
 module.exports.run = client => {
 	console.log(`${client.user.tag} is online!`);
 	client.user.setActivity('Made by RLX'); // Dont remove Made by RLX or I'll sue u!!
+// ----------------------------------------------------------------------------------
+console.log(`Logged in as ${client.user.tag}!`.bgBlue.black)
+console.log(`Discord.js Version: ${Discord.version}`.bgWhite.black)
+console.log(`Node.js Version: ${process.version} `.bgYellow.black);
 };
